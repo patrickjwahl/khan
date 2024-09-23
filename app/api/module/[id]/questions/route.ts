3084eeebf5f7a12ecdf4cc8b6cd79041b4c16763
Patrick <patrickjwahl@gmail.com>
2024-09-23 03:18:59 -0400

    Update API to filter info screens

--- app/api/module/[id]/questions/route.ts
+++ app/api/module/[id]/questions/route.ts
@@ -4,7 +4,16 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest, context: { params: {id: string}}) {
     const moduleId = parseInt(context.params.id);
 
-    const questions = await prisma.question.findMany({where: {moduleId: moduleId}});
+    const questions = await prisma.question.findMany({
+        where: {
+            moduleId: moduleId,
+            type: {
+                not: {
+                    equals: 'INFO'
+                }
+            }
+        }
+    });
 
     return NextResponse.json({questions});
 }
